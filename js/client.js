2c60fbc9e4c1cbce9b8a62aa8d1af015ba9deff5
gongchizhou <gongchizhou@gmail.com>
2017-04-26 17:57:19 +0800

    update

--- js/client.js
+++ js/client.js
@@ -8,12 +8,13 @@ app.client = (function(){
 		},
 
 		onload: function(){
+			alert(app.isTouch);
 			if(!app.isTouch){
 				this.width = 320;
 				this.height = 568;
 				$('body').addClass('pc');
 
-				this.renderClient();
+				//this.renderClient();
 			}else{
 				this.width = window.innerWidth;
 				this.height = window.innerHeight;
